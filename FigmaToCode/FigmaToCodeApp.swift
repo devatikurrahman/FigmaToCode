ee7991adbcd807b7add367433583d196a5068a9b
Atikur Rahman <mratikurrahman@gmail.com>
2024-04-30 16:33:40 +0600

    Initialize project

---
+++ FigmaToCode/FigmaToCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FigmaToCodeApp.swift
+//  FigmaToCode
+//
+//  Created by Atikur Rahman on 4/30/24.
+//
+
+import SwiftUI
+
+@main
+struct FigmaToCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
