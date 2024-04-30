f3f29172c4a350e6cd578dd38dc1ba170eb3f89b
Atikur Rahman <mratikurrahman@gmail.com>
2024-05-01 02:32:38 +0600

    Added task list to the TaskListView

--- FigmaToCode/TaskListView.swift
+++ FigmaToCode/TaskListView.swift
@@ -18,28 +18,53 @@ struct TaskListView: View {
                 .ignoresSafeArea()
             
             ScrollView {
-                VStack {
-                    HStack {
-                        Text("Today")
-                        Spacer()
-                        Text("\(tasks.count)")
-                        Image("ic-downarrow")
-                    }
-                    .font(Font.taskText)
-                    
-                    ForEach(tasks) { task in
+                ZStack {
+                    Rectangle()
+                        .foregroundColor(.white)
+                    VStack {
                         HStack {
-                            Image("ic-checkbox")
-                            Text(task.title)
-                                .font(Font.taskText)
+                            Text("Today")
                             Spacer()
-                            VStack(alignment: .trailing, spacing: 0) {
-                                Text("18.00")
-                                    .foregroundColor(Color("Alarm"))
-                                    .font(Font.taskAlamText)
-                                Image("ic-alarm")
+                            Text("\(tasks.count)")
+                            Image("ic-downarrow")
+                        }
+                        .font(Font.taskText)
+                        
+                        ForEach(tasks) { task in
+                            HStack {
+                                Image("ic-checkbox")
+                                Text(task.title)
+                                    .font(Font.taskText)
+                                Spacer()
+                                VStack(alignment: .trailing, spacing: 0) {
+                                    Text("18.00")
+                                        .foregroundColor(Color("Alarm"))
+                                        .font(Font.taskAlamText)
+                                    Image("ic-alarm")
+                                }
                             }
                         }
+                    }
+                    .padding(.horizontal, 16)
+                    .padding(.vertical, 8)
+                }
+                .padding(.horizontal, 16)
+                .padding(.vertical, 10)
+            }
+            
+            VStack {
+                Spacer()
+                HStack {
+                    Spacer()
+                    Button {
+                        print("Click Plus Btn")
+                    } label: {
+                        Image(systemName: "plus.circle.fill")
+                            .resizable()
+                            .frame(width: 64, height: 64)
+                            .foregroundColor(Color.accentColor)
+                            .padding(.bottom, 28)
+                            .padding(.trailing, 24)
                     }
                 }
             }
